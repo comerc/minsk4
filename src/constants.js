89e96564297f79635452a9773efcc231116cc2b0
comerc <andrew.kachanov@gmail.com>
2019-01-25 21:35:25 +0300

    Added .env.local

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
-export const API = process.env.API || 'http://localhost:4000/'
-export const PAGE_LIMIT = process.env.PAGE_LIMIT || 20
+export const API = process.env.REACT_APP_API || 'http://localhost:4000/'
+export const PAGE_LIMIT = process.env.REACT_APP_PAGE_LIMIT || 20
+export const ACCOUNT_KIT_APP_ID = process.env.REACT_APP_ACCOUNT_KIT_APP_ID || '2022731737796697'
+export const ACCOUNT_KIT_VERSION = process.env.REACT_APP_ACCOUNT_KIT_VERSION || 'v1.0'
