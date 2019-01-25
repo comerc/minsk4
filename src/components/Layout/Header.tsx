fa82c24b1462c23935ab690eb2bee899a2fa2bf4
comerc <andrew.kachanov@gmail.com>
2019-01-25 21:36:18 +0300

    Added AccountKit

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
+import { Button } from 'antd'
+import AccountKit from 'src/components/AccountKit'
 import styled from 'styled-components'
+import { ACCOUNT_KIT_APP_ID, ACCOUNT_KIT_VERSION } from 'src/constants'
 
 const style = () => (Self) => styled(Self)``
 
@@ -7,7 +10,27 @@ const style = () => (Self) => styled(Self)``
 class Header extends Component {
   render() {
     const { className } = this.props as any
-    return <div className={className}>Header</div>
+    return (
+      <div className={className}>
+        <div>Header</div>
+        <AccountKit
+          appId={ACCOUNT_KIT_APP_ID}
+          version={ACCOUNT_KIT_VERSION}
+          onResponse={(resp) => console.log(resp)}
+          csrf={'csrf token here!'}
+          // language="ru_RU"
+          debug={true}
+          display="modal"
+          loginType="EMAIL"
+        >
+          {({ onClick, disabled }) => (
+            <Button {...{ onClick, disabled }} type="primary">
+              Login
+            </Button>
+          )}
+        </AccountKit>
+      </div>
+    )
   }
 }
 
