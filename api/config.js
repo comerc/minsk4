c9ec9a6a556c040afcae726a7d1ba421e5caa3e3
comerc <andrew.kachanov@gmail.com>
2019-03-08 23:46:10 +0300

    Added config for api

---
+++ api/config.js
@@ -0,0 +1,17 @@
+const nconf = (module.exports = require('nconf'))
+const path = require('path')
+
+nconf
+  .file({ format: nconf.formats.ini, file: path.join(__dirname, '../.env.local') })
+  .argv() // read command line args
+  .env([
+    // read environment vars
+    'SECRET',
+    'REACT_APP_ACCOUNT_KIT_APP_ID',
+    'REACT_APP_ACCOUNT_KIT_VERSION',
+    'REACT_APP_HOST',
+  ])
+  .defaults({
+    REACT_APP_ACCOUNT_KIT_VERSION: 'v1.1',
+    REACT_APP_HOST: 'http://localhost:3000',
+  })
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000'
+export const API = process.env.REACT_APP_API || 'http://localhost:5000'
 export const PAGE_LIMIT = process.env.REACT_APP_PAGE_LIMIT || 20
-export const ACCOUNT_KIT_APP_ID = process.env.REACT_APP_ACCOUNT_KIT_APP_ID || '2022731737796697'
-export const ACCOUNT_KIT_VERSION = process.env.REACT_APP_ACCOUNT_KIT_VERSION || 'v1.0'
+export const ACCOUNT_KIT_APP_ID = process.env.REACT_APP_ACCOUNT_KIT_APP_ID
+export const ACCOUNT_KIT_VERSION = process.env.REACT_APP_ACCOUNT_KIT_VERSION || 'v1.1'
