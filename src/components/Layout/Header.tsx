6f0de25cc47474c7369e7030e7c545060214716a
comerc <andrew.kachanov@gmail.com>
2019-03-11 21:12:29 +0300

    Released confirm for logout

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 // import jwt from 'jsonwebtoken'
 import { Link } from 'react-router-dom'
-import { Button } from 'antd'
+import { Menu, Dropdown, Icon, Modal, Button } from 'antd'
 import AccountKit from 'src/components/AccountKit'
 import { API, ACCOUNT_KIT_APP_ID, ACCOUNT_KIT_VERSION } from 'src/constants'
 import styled from 'styled-components'
@@ -11,6 +11,10 @@ const style = () => (Self) => styled(Self)``
 
 @style()
 class Header extends Component {
+  state = {
+    isLogged: !!localStorage.getItem('token'),
+  }
+
   csrf = null
 
   language =
@@ -63,29 +67,77 @@ class Header extends Component {
   //   }
   // }
 
+  logout = (event) => {
+    event.preventDefault()
+    Modal.confirm({
+      title: 'Выйти на всех устройствах?',
+      content: '',
+      okText: 'Да',
+      cancelText: 'Нет, только здесь',
+      onOk() {
+        console.log('handleConfirm')
+      },
+      onCancel() {
+        console.log('handleCancel')
+      },
+    })
+    // const token = localStorage.getItem('token')
+    // if (!token) {
+    //   return
+    // }
+    // axios.post(`${API}/logout`, { token }).then((response) => {
+    //   console.log(response)
+    //   localStorage.removeItem('token')
+    // })
+  }
+
   render() {
     const { className } = this.props as any
+    const { isLogged } = this.state
     return (
       <div className={className}>
         <div>Header</div>
-        <AccountKit
-          {...{
-            appId: ACCOUNT_KIT_APP_ID,
-            version: ACCOUNT_KIT_VERSION,
-            onMount: this.handleAccountKitMount,
-            onLogin: this.handleAccountKitLogin,
-            language: this.language,
-            debug: process.env.NODE_ENV === 'development',
-            loginType: 'EMAIL',
-          }}
-        >
-          {({ onClick, disabled }) => (
-            <Button {...{ onClick, disabled }} type="primary">
-              Login
-            </Button>
-          )}
-        </AccountKit>
         <Link to="/404">404</Link>
+        <br />
+        {isLogged && (
+          <Dropdown
+            overlay={
+              <Menu>
+                <Menu.Item key="0">
+                  <a href="#" onClick={this.logout}>
+                    Logout
+                  </a>
+                </Menu.Item>
+                <Menu.Divider />
+                <Menu.Item key="3">3rd menu item</Menu.Item>
+              </Menu>
+            }
+            trigger={['click']}
+          >
+            <a className="ant-dropdown-link" href="#">
+              Profile <Icon type="down" />
+            </a>
+          </Dropdown>
+        )}
+        {isLogged || (
+          <AccountKit
+            {...{
+              appId: ACCOUNT_KIT_APP_ID,
+              version: ACCOUNT_KIT_VERSION,
+              onMount: this.handleAccountKitMount,
+              onLogin: this.handleAccountKitLogin,
+              language: this.language,
+              debug: process.env.NODE_ENV === 'development',
+              loginType: 'EMAIL',
+            }}
+          >
+            {({ onClick, disabled }) => (
+              <Button {...{ onClick, disabled }} type="primary">
+                Login
+              </Button>
+            )}
+          </AccountKit>
+        )}
       </div>
     )
   }
