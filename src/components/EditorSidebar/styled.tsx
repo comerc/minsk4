f3a6d10358d16b9957a9a607d05bc4e5d717337d
comerc <andrew.kachanov@gmail.com>
2019-04-26 21:07:38 +0300

    Removed unused styles (vs autoprefixer styles)

--- src/components/EditorSidebar/styled.tsx
+++ src/components/EditorSidebar/styled.tsx
@@ -11,19 +11,10 @@ export const SidebarContainer = styled.div`
   z-index: 5;
   i {
     color: #ccc;
-    -webkit-animation: fadeIn 1s; /* Safari, Chrome and Opera > 12.1 */
-    -moz-animation: fadeIn 1s; /* Firefox < 16 */
-    -ms-animation: fadeIn 1s; /* Internet Explorer */
-    -o-animation: fadeIn 1s; /* Opera < 12.1 */
     animation: fadeIn 1s;
   }
   i.open {
-    -webkit-animation: spin 1s; /* Safari, Chrome and Opera > 12.1 */
-    -moz-animation: spin 1s; /* Firefox < 16 */
-    -ms-animation: spin 1s; /* Internet Explorer */
-    -o-animation: spin 1s; /* Opera < 12.1 */
     animation: spin 1s;
-    -webkit-animation-fill-mode: forwards; /* Safari 4.0 - 8.0 */
     animation-fill-mode: forwards;
   }
   @keyframes fadeIn {
@@ -32,38 +23,6 @@ export const SidebarContainer = styled.div`
     }
     to {
       opacity: 1;
-    }
-  }
-  @-webkit-keyframes fadeIn {
-    0% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-  }
-  @-moz-keyframes fadeIn {
-    0% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-  }
-  @-moz-keyframes spin {
-    from {
-      -moz-transform: rotate(0deg);
-    }
-    to {
-      -moz-transform: rotate(45deg);
-    }
-  }
-  @-webkit-keyframes spin {
-    from {
-      -webkit-transform: rotate(0deg);
-    }
-    to {
-      -webkit-transform: rotate(45deg);
     }
   }
   @keyframes spin {
@@ -93,7 +52,7 @@ export const IconContainer = styled.div`
   padding: 3px 5px;
   width: 100px;
   text-align: center;
-  .__slate-sidebar_slateToolbarItem {
+  /* .__slate-sidebar_slateToolbarItem {
     height: 40px;
     width: 40px;
   }
@@ -149,7 +108,7 @@ export const IconContainer = styled.div`
     stroke: #333;
     fill: transparent;
     stroke-width: 1;
-  }
+  } */
 `
 
 export const IconWrapper = styled.div`
