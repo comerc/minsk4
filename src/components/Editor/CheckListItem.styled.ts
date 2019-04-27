ac80a50fb7d1045c26f2f411d7b76ffc2407563a
comerc <andrew.kachanov@gmail.com>
2019-04-27 20:40:51 +0300

    Refactored styled

--- src/components/Editor/CheckListItem.styled.ts
+++ src/components/Editor/CheckListItem.styled.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const ItemWrapper = styled('div')`
+export const ItemWrapper = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -9,11 +9,11 @@ export const ItemWrapper = styled('div')`
   }
 `
 
-export const CheckboxWrapper = styled('span')`
+export const CheckboxWrapper = styled.span`
   margin-right: 0.75em;
 `
 
-export const ContentWrapper = styled('span')`
+export const ContentWrapper = styled.span`
   flex: 1;
   opacity: ${(props) => (props.checked ? 0.62 : 1)};
   text-decoration: ${(props) => (props.checked ? 'line-through' : 'none')};
