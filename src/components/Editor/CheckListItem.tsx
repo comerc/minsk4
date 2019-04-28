22b108c1155cffcde5fd8f17b89166efbc62c35c
comerc <andrew.kachanov@gmail.com>
2019-04-28 12:13:26 +0300

    Moved styles from .styled.ts

--- src/components/Editor/CheckListItem.tsx
+++ src/components/Editor/CheckListItem.tsx
@@ -1,6 +1,27 @@
 import React from 'react'
-import { ItemWrapper, CheckboxWrapper, ContentWrapper } from './CheckListItem.styled'
+import styled, { css } from 'styled-components'
 
+const style = () => (Self) => styled(Self)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  & + & {
+    margin-top: 0;
+  }
+  .checkbox-wrapper {
+    margin-right: 0.75em;
+  }
+  .content-wrapper {
+    flex: 1;
+    opacity: ${(props) => (props.node.data.get('checked') ? 0.62 : 1)};
+    text-decoration: ${(props) => (props.node.data.get('checked') ? 'line-through' : 'none')};
+    &:focus {
+      outline: none;
+    }
+  }
+`
+
+@style()
 class CheckListItem extends React.Component {
   onChange = (event) => {
     const checked = event.target.checked
@@ -9,21 +30,21 @@ class CheckListItem extends React.Component {
   }
 
   render() {
-    const { attributes, children, node, readOnly } = this.props as any
+    const { className, attributes, children, node, readOnly } = this.props as any
     const checked = node.data.get('checked')
     return (
-      <ItemWrapper {...attributes}>
-        <CheckboxWrapper contentEditable={false}>
+      <div {...attributes} className={className}>
+        <span className="checkbox-wrapper" contentEditable={false}>
           <input type="checkbox" checked={checked} onChange={this.onChange} />
-        </CheckboxWrapper>
-        <ContentWrapper
-          checked={checked}
+        </span>
+        <span
+          className="content-wrapper"
           contentEditable={!readOnly}
           suppressContentEditableWarning
         >
           {children}
-        </ContentWrapper>
-      </ItemWrapper>
+        </span>
+      </div>
     )
   }
 }
