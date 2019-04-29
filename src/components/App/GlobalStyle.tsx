c968786c58b06b1b5e994a15ea0a78c1460f5523
comerc <andrew.kachanov@gmail.com>
2019-04-29 22:58:59 +0300

    Added all styles for .ce-toolbar

--- src/components/App/GlobalStyle.tsx
+++ src/components/App/GlobalStyle.tsx
@@ -1,7 +1,29 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const theme = {
-  mobileSize: 650,
+  mobile: '(max-width: 650px)',
+  notMobile: '(min-width: 651px)',
+  /**
+   * Block content width
+   * Should be set in a constant at the modules/ui.js
+   */
+  contentWidth: '650px',
+  /**
+   * Toolbar buttons height and width
+   */
+  toolboxButtonsSize: '34px',
+  /**
+   * All gray texts: placeholders, settings
+   */
+  grayText: '#707684',
+  /**
+   * Blue icons
+   */
+  colorActiveIcon: '#388AE5',
+  /**
+   * Toolbar buttons
+   */
+  bgLight: '#eff2f5',
 }
 
 const GlobalStyle = createGlobalStyle``
