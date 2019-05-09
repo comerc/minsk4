85471d1d19024fb3cf3ce89a9957a9115aa6448a
comerc <andrew.kachanov@gmail.com>
2019-05-09 16:50:07 +0300

    Fixed box-shadow for .ant-tooltip-arrow

--- src/packages/slate-toolbar/Settings.tsx
+++ src/packages/slate-toolbar/Settings.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 import styled from 'styled-components'
 import { Tooltip } from 'antd'
 
+/* TODO: move variables to theme */
 const arrowWidth = 6
 const sqrtArrowWidth = Math.sqrt(arrowWidth * arrowWidth * 2)
+const top = 4
+const right = 16
+const backgroundColor = '#fff'
 
 const withStyle = (Self) => styled(Self)`
   .ant-tooltip-arrow {
@@ -11,17 +15,29 @@ const withStyle = (Self) => styled(Self)`
     height: ${sqrtArrowWidth}px !important;
     border-width: ${sqrtArrowWidth / 2}px !important;
     transform: rotate(45deg);
-    top: 4px !important;
-    border-top-color: #fff !important;
+    top: ${top}px !important;
+    right: ${right}px !important;
+    border-top-color: transparent !important;
     border-right-color: transparent !important;
     border-bottom-color: transparent !important;
-    border-left-color: #fff !important;
+    border-left-color: transparent !important;
     box-shadow: -2px -2px 5px rgba(0, 0, 0, 0.06);
   }
   .ant-tooltip-inner {
     color: rgba(0, 0, 0, 0.65);
-    background-color: #fff;
+    background-color: ${backgroundColor};
     background-clip: padding-box;
+    position: relative;
+    &::before {
+      content: '';
+      position: absolute;
+      top: -${top}px;
+      right: ${right}px;
+      width: ${sqrtArrowWidth}px;
+      height: ${sqrtArrowWidth}px;
+      background-color: ${backgroundColor};
+      transform: rotate(45deg);
+    }
   }
 `
 
