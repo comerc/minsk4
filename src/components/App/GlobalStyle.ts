d3fec778aac9ef17642cf3d684fc737d65f1d6e4
comerc <andrew.kachanov@gmail.com>
2019-05-10 14:37:13 +0300

    Moved src/theme

--- src/components/App/GlobalStyle.ts
+++ src/components/App/GlobalStyle.ts
@@ -1,31 +1,9 @@
 import { createGlobalStyle } from 'styled-components'
+import theme from 'src/theme'
 
-export const theme = {
-  mobile: '(max-width: 650px)',
-  notMobile: '(min-width: 651px)',
-  /**
-   * Block content width
-   * Should be set in a constant at the modules/ui.js
-   */
-  contentWidth: '650px',
-  /**
-   * Toolbar buttons height and width
-   */
-  toolboxButtonsSize: '34px',
-  /**
-   * All gray texts: placeholders, settings
-   */
-  grayText: '#707684',
-  /**
-   * Blue icons
-   */
-  colorActiveIcon: '#388AE5',
-  /**
-   * Background Color
-   */
-  backgroundColor: '#FFF',
-}
+// console.log(theme)
 
 const GlobalStyle = createGlobalStyle``
 
+export { theme }
 export default GlobalStyle
---
+++ src/theme.js
@@ -0,0 +1,35 @@
+const theme = {
+  mobile: '(max-width: 650px)',
+  notMobile: '(min-width: 651px)',
+  /**
+   * Block content width
+   * Should be set in a constant at the modules/ui.js
+   */
+  contentWidth: '650px',
+  /**
+   * Toolbar buttons height and width
+   */
+  toolboxButtonsSize: '34px',
+  /**
+   * All gray texts: placeholders, settings
+   */
+  grayText: '#707684',
+  /**
+   * Blue icons
+   */
+  colorActiveIcon: '#388ae5',
+  /**
+   * Background Color
+   */
+  // backgroundColor: '#fff',
+  /**
+   * Primary Color
+   */
+  // primaryColor: 'red',
+  /**
+   * Popover Background Color (Antd)
+   */
+  popoverBg: '#fff',
+}
+
+export default theme
