ca0e556c2ebdfbd1e5efedaa03db031b923dcc2d
comerc <andrew.kachanov@gmail.com>
2019-06-08 16:51:30 +0300

    Formatted code

--- config-overrides.js
+++ config-overrides.js
@@ -21,22 +21,17 @@ const createRewireBabel = (modifyOptions) => {
 const createRewireLess = (options = {}) => {
   return (config, env) => {
     const oneOf = config.module.rules.find((element) => element.oneOf).oneOf
-
     const lessExtension = /\.less$/
-
     const fileLoader = oneOf.find(
       (element) => element.loader && element.loader.includes('file-loader'),
     )
     fileLoader.exclude.push(lessExtension)
-
     const testSource = /\.css$/.source
     const cssRules = oneOf.find((element) => element.test.source === testSource)
-
     const lessRules = {
       test: lessExtension,
       use: [...cssRules.use, { loader: 'less-loader', options }],
     }
-
     oneOf.unshift(lessRules)
   }
 }
