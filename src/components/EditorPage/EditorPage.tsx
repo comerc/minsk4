0f468caee2366243954cbd6bceac1dd5c1205fb7
comerc <andrew.kachanov@gmail.com>
2019-06-26 20:25:18 +0300

    Added Select for choice language in code-block

--- src/components/EditorPage/EditorPage.tsx
+++ src/components/EditorPage/EditorPage.tsx
@@ -1,20 +1,43 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Input } from 'antd'
+import { Input, Select } from 'antd'
 import Editor from 'src/components/Editor'
+
+const { Option } = Select
 
 const withStyle = (Self) => styled(Self)`
   ${Editor} {
     margin-top: 8px;
   }
+  .language {
+    min-width: 100px;
+  }
 `
 
 @withStyle
 class EditorPage extends React.Component<any> {
+  filterOption = (input, option) => {
+    return (option.props.children as any).toLowerCase().indexOf(input.toLowerCase()) >= 0
+  }
+
   render() {
     const { className } = this.props
     return (
       <div className={className}>
+        <Select
+          {...{
+            className: 'language',
+            defaultValue: 'none',
+            showSearch: true,
+            dropdownMatchSelectWidth: false,
+            optionFilterProp: 'children',
+            filterOption: this.filterOption,
+          }}
+        >
+          <Option value="none">(none)</Option>
+          <Option value="js">JavaScript</Option>
+          <Option value="html">HTML</Option>
+        </Select>
         <Input.TextArea
           {...{
             autosize: true,
