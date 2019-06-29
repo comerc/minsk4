9e238bcecef9850a8a18426009ae942caabbbd21
comerc <andrew.kachanov@gmail.com>
2019-06-29 18:24:17 +0300

    Added slate-coder

--- support/rollup/config.js
+++ support/rollup/config.js
@@ -1,6 +1,7 @@
 import factory from './factory'
+import slateCoder from '../../packages/slate-coder/package.json'
 import slateControls from '../../packages/slate-controls/package.json'
 
-const configurations = [...factory(slateControls)]
+const configurations = [...factory(slateCoder), ...factory(slateControls)]
 
 export default configurations
